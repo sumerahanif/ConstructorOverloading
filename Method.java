b65e108245e1516d7cfaad0f93880c1bcd8f952d
sumerahanif <sumera3687@yahoo.com>
2017-03-18 11:00:40 +0500

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,37 @@
+class BOX
+{
+double height,width,depth;
+public BOX(double height,double width,double depth,)
+{
+height=height
+depth=depth
+width=width
+}
+public BOX(double length)
+{
+height=depht=width=length;
+}
+public double get volume ()
+{
+ double volume=height*width*depht;
+ return volume;
+ }
+ public class MyClass
+ {
+ public static void main (String[]args)
+ {
+ Box box=new Box(3,2,4);
+ systm.out.println("volume for Box="+box.getvolume());
+ BOX cube =new BOx(2.5);
+  systm.out.println("volume for Cube="+Cube.getvolume());
+  }
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
